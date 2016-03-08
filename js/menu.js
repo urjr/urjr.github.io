d7c3e034e4974e8d5948a56cb948ceab257bfea5
urjr <urjr8891@gmail.com>
2016-03-07 23:32:10 -0500

    added menu  toggle to website

--- js/menu.js
+++ js/menu.js
@@ -1,9 +1,33 @@
 $(function(){
-	$('.bar-menu').on('click', function(){
-		$('.menu').toggleClass('disabled');
-	});
 
-	$('.menu-close').on('click', function(){
-		$('.menu').toggleClass('disabled');
-	});
+	var menu = {
+		isOpen: false,
+
+		toggle: function(){
+			$('.menu').toggleClass('disabled');
+		},
+
+		init: function(){
+			var $openBtn = $('.bar-menu');
+			var $closeBtn = $('.menu-close');
+			if (menu.isOpen) {
+				$closeBtn.on('click', function(){
+					menu.toggle();
+					menu.isOpen = !menu.isOpen;
+					$closeBtn.off();
+					menu.init();
+				});
+			} else {
+				$openBtn.on('click', function(){
+					menu.toggle();
+					menu.isOpen = !menu.isOpen;
+					$openBtn.off();
+					menu.init();
+				});
+			};
+		},
+	};
+
+	menu.init();
+
 });
