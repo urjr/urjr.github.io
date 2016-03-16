38c6590e692c88cc12194e2f13e41e926a78cc38
urjr <urjr8891@gmail.com>
2016-03-16 10:41:05 -0400

    added adhawk animation and resized elements in the menu

--- js/placeholder.js
+++ js/placeholder.js
@@ -1,6 +1,6 @@
 $(function(){
 	$('.placeholder').tooltip({
-		title: "Come visit me soon, I'll be a website by then. I promise!",
+		title: "Come visit me again soon, I'll be a website by then. I promise!",
 		placement: 'bottom'
 	});
 });
