ab312f350179072b43e5c64b2a4d8175c7d07669
Ulises Reyes <ulises@notarize.com>
2019-01-23 14:44:37 -0500

    added mapdata js file

---
+++ js/mapdata.js
@@ -0,0 +1,1016 @@
+var simplemaps_worldmap_mapdata={
+  main_settings: {
+   //General settings
+    width: "responsive", //'700' or 'responsive'
+    background_color: "#FFFFFF",
+    background_transparent: "yes",
+    border_color: "#ffffff",
+    popups: "detect",
+    
+    //State defaults
+    state_description: "State description",
+    state_color: "#88A4BC",
+    state_hover_color: "#3B729F",
+    state_url: "http://simplemaps.com",
+    border_size: 1.5,
+    all_states_inactive: "no",
+    all_states_zoomable: "no",
+    
+    //Location defaults
+    location_description: "Location description",
+    location_color: "#FF0067",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_url: "",
+    location_size: 25,
+    location_type: "heart",
+    location_image_source: "frog.png",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    
+    //Label defaults
+    label_color: "#d5ddec",
+    label_hover_color: "#d5ddec",
+    label_size: 22,
+    label_font: "Arial",
+    hide_labels: "no",
+   
+    //Zoom settings
+    zoom: "yes",
+    back_image: "no",
+    initial_back: "no",
+    initial_zoom: -1,
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+    //Popup settings
+    popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+    //Advanced settings
+    div: "map",
+    auto_load: "yes",
+    url_new_tab: "yes",
+    images_directory: "default",
+    fade_time: 0.1,
+    link_text: "View Website",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: ""
+  },
+  state_specific: {
+    AF: {
+      name: "Afghanistan"
+    },
+    AO: {
+      name: "Angola"
+    },
+    AL: {
+      name: "Albania"
+    },
+    AE: {
+      name: "United Arab Emirates"
+    },
+    AR: {
+      name: "Argentina"
+    },
+    AM: {
+      name: "Armenia"
+    },
+    AU: {
+      name: "Australia"
+    },
+    AT: {
+      name: "Austria"
+    },
+    AZ: {
+      name: "Azerbaijan"
+    },
+    BI: {
+      name: "Burundi"
+    },
+    BE: {
+      name: "Belgium"
+    },
+    BJ: {
+      name: "Benin"
+    },
+    BF: {
+      name: "Burkina Faso"
+    },
+    BD: {
+      name: "Bangladesh"
+    },
+    BG: {
+      name: "Bulgaria"
+    },
+    BH: {
+      name: "Bahrain"
+    },
+    BA: {
+      name: "Bosnia and Herzegovina"
+    },
+    BY: {
+      name: "Belarus"
+    },
+    BZ: {
+      name: "Belize"
+    },
+    BO: {
+      name: "Bolivia"
+    },
+    BR: {
+      name: "Brazil"
+    },
+    BN: {
+      name: "Brunei Darussalam"
+    },
+    BT: {
+      name: "Bhutan"
+    },
+    BW: {
+      name: "Botswana"
+    },
+    CF: {
+      name: "Central African Republic"
+    },
+    CA: {
+      name: "Canada"
+    },
+    CH: {
+      name: "Switzerland"
+    },
+    CL: {
+      name: "Chile"
+    },
+    CN: {
+      name: "China"
+    },
+    CI: {
+      name: "Côte d'Ivoire"
+    },
+    CM: {
+      name: "Cameroon"
+    },
+    CD: {
+      name: "Democratic Republic of the Congo"
+    },
+    CG: {
+      name: "Republic of Congo"
+    },
+    CO: {
+      name: "Colombia"
+    },
+    CR: {
+      name: "Costa Rica"
+    },
+    CU: {
+      name: "Cuba"
+    },
+    CZ: {
+      name: "Czech Republic"
+    },
+    DE: {
+      name: "Germany"
+    },
+    DJ: {
+      name: "Djibouti"
+    },
+    DK: {
+      name: "Denmark"
+    },
+    DO: {
+      name: "Dominican Republic"
+    },
+    DZ: {
+      name: "Algeria"
+    },
+    EC: {
+      name: "Ecuador"
+    },
+    EG: {
+      name: "Egypt"
+    },
+    ER: {
+      name: "Eritrea"
+    },
+    EE: {
+      name: "Estonia"
+    },
+    ET: {
+      name: "Ethiopia"
+    },
+    FI: {
+      name: "Finland"
+    },
+    FJ: {
+      name: "Fiji"
+    },
+    GA: {
+      name: "Gabon"
+    },
+    GB: {
+      name: "United Kingdom"
+    },
+    GE: {
+      name: "Georgia"
+    },
+    GH: {
+      name: "Ghana"
+    },
+    GN: {
+      name: "Guinea"
+    },
+    GM: {
+      name: "The Gambia"
+    },
+    GW: {
+      name: "Guinea-Bissau"
+    },
+    GQ: {
+      name: "Equatorial Guinea"
+    },
+    GR: {
+      name: "Greece"
+    },
+    GL: {
+      name: "Greenland"
+    },
+    GT: {
+      name: "Guatemala"
+    },
+    GY: {
+      name: "Guyana"
+    },
+    HN: {
+      name: "Honduras"
+    },
+    HR: {
+      name: "Croatia"
+    },
+    HT: {
+      name: "Haiti"
+    },
+    HU: {
+      name: "Hungary"
+    },
+    ID: {
+      name: "Indonesia"
+    },
+    IN: {
+      name: "India"
+    },
+    IE: {
+      name: "Ireland"
+    },
+    IR: {
+      name: "Iran"
+    },
+    IQ: {
+      name: "Iraq"
+    },
+    IS: {
+      name: "Iceland"
+    },
+    IL: {
+      name: "Israel"
+    },
+    IT: {
+      name: "Italy"
+    },
+    JM: {
+      name: "Jamaica"
+    },
+    JO: {
+      name: "Jordan"
+    },
+    JP: {
+      name: "Japan"
+    },
+    KZ: {
+      name: "Kazakhstan"
+    },
+    KE: {
+      name: "Kenya"
+    },
+    KG: {
+      name: "Kyrgyzstan"
+    },
+    KH: {
+      name: "Cambodia"
+    },
+    KR: {
+      name: "Republic of Korea"
+    },
+    XK: {
+      name: "Kosovo"
+    },
+    KW: {
+      name: "Kuwait"
+    },
+    LA: {
+      name: "Lao PDR"
+    },
+    LB: {
+      name: "Lebanon"
+    },
+    LR: {
+      name: "Liberia"
+    },
+    LY: {
+      name: "Libya"
+    },
+    LK: {
+      name: "Sri Lanka"
+    },
+    LS: {
+      name: "Lesotho"
+    },
+    LT: {
+      name: "Lithuania"
+    },
+    LU: {
+      name: "Luxembourg"
+    },
+    LV: {
+      name: "Latvia"
+    },
+    MA: {
+      name: "Morocco"
+    },
+    MD: {
+      name: "Moldova"
+    },
+    MG: {
+      name: "Madagascar"
+    },
+    MX: {
+      name: "Mexico"
+    },
+    MK: {
+      name: "Macedonia"
+    },
+    ML: {
+      name: "Mali"
+    },
+    MM: {
+      name: "Myanmar"
+    },
+    ME: {
+      name: "Montenegro"
+    },
+    MN: {
+      name: "Mongolia"
+    },
+    MZ: {
+      name: "Mozambique"
+    },
+    MR: {
+      name: "Mauritania"
+    },
+    MW: {
+      name: "Malawi"
+    },
+    MY: {
+      name: "Malaysia"
+    },
+    NA: {
+      name: "Namibia"
+    },
+    NE: {
+      name: "Niger"
+    },
+    NG: {
+      name: "Nigeria"
+    },
+    NI: {
+      name: "Nicaragua"
+    },
+    NL: {
+      name: "Netherlands"
+    },
+    NO: {
+      name: "Norway"
+    },
+    NP: {
+      name: "Nepal"
+    },
+    NZ: {
+      name: "New Zealand"
+    },
+    OM: {
+      name: "Oman"
+    },
+    PK: {
+      name: "Pakistan"
+    },
+    PA: {
+      name: "Panama"
+    },
+    PE: {
+      name: "Peru"
+    },
+    PH: {
+      name: "Philippines"
+    },
+    PG: {
+      name: "Papua New Guinea"
+    },
+    PL: {
+      name: "Poland"
+    },
+    KP: {
+      name: "Dem. Rep. Korea"
+    },
+    PT: {
+      name: "Portugal"
+    },
+    PY: {
+      name: "Paraguay"
+    },
+    PS: {
+      name: "Palestine"
+    },
+    QA: {
+      name: "Qatar"
+    },
+    RO: {
+      name: "Romania"
+    },
+    RU: {
+      name: "Russia"
+    },
+    RW: {
+      name: "Rwanda"
+    },
+    EH: {
+      name: "Western Sahara"
+    },
+    SA: {
+      name: "Saudi Arabia"
+    },
+    SD: {
+      name: "Sudan"
+    },
+    SS: {
+      name: "South Sudan"
+    },
+    SN: {
+      name: "Senegal"
+    },
+    SL: {
+      name: "Sierra Leone"
+    },
+    SV: {
+      name: "El Salvador"
+    },
+    RS: {
+      name: "Serbia"
+    },
+    SR: {
+      name: "Suriname"
+    },
+    SK: {
+      name: "Slovakia"
+    },
+    SI: {
+      name: "Slovenia"
+    },
+    SE: {
+      name: "Sweden"
+    },
+    SZ: {
+      name: "Swaziland"
+    },
+    SY: {
+      name: "Syria"
+    },
+    TD: {
+      name: "Chad"
+    },
+    TG: {
+      name: "Togo"
+    },
+    TH: {
+      name: "Thailand"
+    },
+    TJ: {
+      name: "Tajikistan"
+    },
+    TM: {
+      name: "Turkmenistan"
+    },
+    TL: {
+      name: "Timor-Leste"
+    },
+    TN: {
+      name: "Tunisia"
+    },
+    TR: {
+      name: "Turkey"
+    },
+    TW: {
+      name: "Taiwan"
+    },
+    TZ: {
+      name: "Tanzania"
+    },
+    UG: {
+      name: "Uganda"
+    },
+    UA: {
+      name: "Ukraine"
+    },
+    UY: {
+      name: "Uruguay"
+    },
+    US: {
+      name: "United States"
+    },
+    UZ: {
+      name: "Uzbekistan"
+    },
+    VE: {
+      name: "Venezuela"
+    },
+    VN: {
+      name: "Vietnam"
+    },
+    VU: {
+      name: "Vanuatu"
+    },
+    YE: {
+      name: "Yemen"
+    },
+    ZA: {
+      name: "South Africa"
+    },
+    ZM: {
+      name: "Zambia"
+    },
+    ZW: {
+      name: "Zimbabwe"
+    },
+    SO: {
+      name: "Somalia"
+    },
+    GF: {
+      name: "France"
+    },
+    FR: {
+      name: "France"
+    },
+    ES: {
+      name: "Spain"
+    },
+    AW: {
+      name: "Aruba"
+    },
+    AI: {
+      name: "Anguilla"
+    },
+    AD: {
+      name: "Andorra"
+    },
+    AG: {
+      name: "Antigua and Barbuda"
+    },
+    BS: {
+      name: "Bahamas"
+    },
+    BM: {
+      name: "Bermuda"
+    },
+    BB: {
+      name: "Barbados"
+    },
+    KM: {
+      name: "Comoros"
+    },
+    CV: {
+      name: "Cape Verde"
+    },
+    KY: {
+      name: "Cayman Islands"
+    },
+    DM: {
+      name: "Dominica"
+    },
+    FK: {
+      name: "Falkland Islands"
+    },
+    FO: {
+      name: "Faeroe Islands"
+    },
+    GD: {
+      name: "Grenada"
+    },
+    HK: {
+      name: "Hong Kong"
+    },
+    KN: {
+      name: "Saint Kitts and Nevis"
+    },
+    LC: {
+      name: "Saint Lucia"
+    },
+    LI: {
+      name: "Liechtenstein"
+    },
+    MF: {
+      name: "Saint Martin (French)"
+    },
+    MV: {
+      name: "Maldives"
+    },
+    MT: {
+      name: "Malta"
+    },
+    MS: {
+      name: "Montserrat"
+    },
+    MU: {
+      name: "Mauritius"
+    },
+    NC: {
+      name: "New Caledonia"
+    },
+    NR: {
+      name: "Nauru"
+    },
+    PN: {
+      name: "Pitcairn Islands"
+    },
+    PR: {
+      name: "Puerto Rico"
+    },
+    PF: {
+      name: "French Polynesia"
+    },
+    SG: {
+      name: "Singapore"
+    },
+    SB: {
+      name: "Solomon Islands"
+    },
+    ST: {
+      name: "São Tomé and Principe"
+    },
+    SX: {
+      name: "Saint Martin (Dutch)"
+    },
+    SC: {
+      name: "Seychelles"
+    },
+    TC: {
+      name: "Turks and Caicos Islands"
+    },
+    TO: {
+      name: "Tonga"
+    },
+    TT: {
+      name: "Trinidad and Tobago"
+    },
+    VC: {
+      name: "Saint Vincent and the Grenadines"
+    },
+    VG: {
+      name: "British Virgin Islands"
+    },
+    VI: {
+      name: "United States Virgin Islands"
+    },
+    CY: {
+      name: "Cyprus"
+    },
+    RE: {
+      name: "Reunion (France)"
+    },
+    YT: {
+      name: "Mayotte (France)"
+    },
+    MQ: {
+      name: "Martinique (France)"
+    },
+    GP: {
+      name: "Guadeloupe (France)"
+    },
+    CW: {
+      name: "Curaco (Netherlands)"
+    },
+    IC: {
+      name: "Canary Islands (Spain)"
+    }
+  },
+  locations: {
+    "0": {
+      lat: 40.439,
+      lng: -79.997,
+      name: "Pittburgh, Pennsylvania",
+      description: "Priyanka and Ulises met in 2011, when Priyanka was a student at the University of Pittsburgh. They loved visiting the Carnegie Library and the Cathedral of Learning."
+    },
+    "1": {
+      lat: 42.359,
+      lng: -71.057,
+      name: "Boston, Massachusetts",
+      description: "Ulises was a student at MIT for the first four years of Ulises and Priyanka’s relationship. He never predicted that he would return there for another two years when it was Priyanka’s turn to be a student at Harvard!"
+    },
+    "2": {
+      lat: 40.715,
+      lng: -74.007,
+      name: "New York City, New York",
+      description: "Ulises spent his early years in the Bronx before moving to Miami.\nPriyanka moved to Brooklyn, New York in 2013 to become a middle school reading teacher. Two years later, Ulises joined her in New York. New York is special to them because it is the first city that they lived in together!"
+    },
+    "3": {
+      lat: 37.777,
+      lng: -122.42,
+      name: "San Francisco, CA",
+      description: "In the early part of 2015, Ulises lived in the Bay Area working his first job out of grad school. After 6 months with the company, Ulises packed his bags and moved to New York, where he was reunited with Priyanka. (New York was the first city they lived together in!)"
+    },
+    "6": {
+      lat: 18.472,
+      lng: -69.891,
+      name: "Santo Domingo, Dominican Republic",
+      description: "Half of Ulises’ family comes from the Dominican Republic. Fun fact: Ulises shares his first name with his father."
+    },
+    "7": {
+      lat: 54.978,
+      lng: -1.612,
+      name: "England, UK",
+      description: "Priyanka was born in England, and most of her family still lives there. In July 2018, Ulises got to visit England for Priyanka’s grandma’s 90th birthday!"
+    },
+    "8": {
+      lat: 18.472,
+      lng: -66.72,
+      name: "Arecibo, Puerto Rico",
+      description: "Half of Ulises’ family comes from Puerto Rico, which is part of the United States. He was born there."
+    },
+    "9": {
+      lat: 30.843,
+      lng: 75.417,
+      name: "Punjab, India",
+      description: "Priyanka’s family comes from Punjab, in northern India. She has never been there, but Priyanka and Ulises hope to visit one day! Many of the beautiful traditions you will see at our wedding come from India."
+    },
+    "10": {
+      lat: 25.775,
+      lng: -80.198,
+      name: "Miami, Florida",
+      description: "Ulises was raised in Miami. Most of his family still lives there, and we are thrilled to host you in this beautiful city!"
+    },
+    "11": {
+      lat: 55.597,
+      lng: 13.002,
+      name: "Malmö, Sweden",
+      description: "In 2012, Priyanka lived in Sweden for 8 months to study and conduct research. Ulises visited her - it was his first time going to Europe. They have celebrated the Swedish holiday of midsummer twice - in 2012 and 2018."
+    }
+  },
+  labels: {},
+  regions: {
+    "0": {
+      name: "North America",
+      states: [
+        "MX",
+        "CA",
+        "US",
+        "GL",
+        "BM"
+      ]
+    },
+    "1": {
+      name: "South America",
+      states: [
+        "EC",
+        "AR",
+        "VE",
+        "BR",
+        "CO",
+        "BO",
+        "PE",
+        "BZ",
+        "CL",
+        "CR",
+        "CU",
+        "DO",
+        "SV",
+        "GT",
+        "GY",
+        "GF",
+        "HN",
+        "NI",
+        "PA",
+        "PY",
+        "PR",
+        "SR",
+        "UY",
+        "JM",
+        "HT",
+        "BS",
+        "FK",
+        "AI",
+        "AG",
+        "AW",
+        "BB",
+        "VG",
+        "KY",
+        "DM",
+        "MQ",
+        "LC",
+        "VC",
+        "GD",
+        "GP",
+        "MS",
+        "TC",
+        "SX",
+        "MF",
+        "KN",
+        "CW"
+      ]
+    },
+    "2": {
+      name: "Europe",
+      states: [
+        "IT",
+        "NL",
+        "NO",
+        "DK",
+        "IE",
+        "GB",
+        "RO",
+        "DE",
+        "FR",
+        "AL",
+        "AM",
+        "AT",
+        "BY",
+        "BE",
+        "LU",
+        "BG",
+        "CZ",
+        "EE",
+        "GE",
+        "GR",
+        "HU",
+        "IS",
+        "LV",
+        "LT",
+        "MD",
+        "PL",
+        "PT",
+        "RS",
+        "SI",
+        "HR",
+        "BA",
+        "ME",
+        "MK",
+        "SK",
+        "ES",
+        "FI",
+        "SE",
+        "CH",
+        "TR",
+        "CY",
+        "UA",
+        "XK",
+        "MT",
+        "FO"
+      ]
+    },
+    "3": {
+      name: "Africa and the Middle East",
+      states: [
+        "QA",
+        "BH",
+        "SA",
+        "AE",
+        "SY",
+        "OM",
+        "KW",
+        "PK",
+        "AZ",
+        "AF",
+        "IR",
+        "IQ",
+        "IL",
+        "PS",
+        "JO",
+        "LB",
+        "YE",
+        "TJ",
+        "TM",
+        "UZ",
+        "KG",
+        "NE",
+        "AO",
+        "EG",
+        "TN",
+        "GA",
+        "DZ",
+        "LY",
+        "CG",
+        "GQ",
+        "BJ",
+        "BW",
+        "BF",
+        "BI",
+        "CM",
+        "CF",
+        "TD",
+        "CI",
+        "CD",
+        "DJ",
+        "ET",
+        "GM",
+        "GH",
+        "GN",
+        "GW",
+        "KE",
+        "LS",
+        "LR",
+        "MG",
+        "MW",
+        "ML",
+        "MA",
+        "MR",
+        "MZ",
+        "NA",
+        "NG",
+        "ER",
+        "RW",
+        "SN",
+        "SL",
+        "SO",
+        "ZA",
+        "SD",
+        "SS",
+        "SZ",
+        "TZ",
+        "TG",
+        "UG",
+        "EH",
+        "ZM",
+        "ZW",
+        "RE",
+        "KM",
+        "SC",
+        "MU",
+        "CV",
+        "IC",
+        "ST",
+        "YT"
+      ]
+    },
+    "4": {
+      name: "South Asia",
+      states: [
+        "SG",
+        "TW",
+        "IN",
+        "AU",
+        "MY",
+        "NP",
+        "NZ",
+        "TH",
+        "BN",
+        "JP",
+        "VN",
+        "LK",
+        "SB",
+        "FJ",
+        "BD",
+        "BT",
+        "KH",
+        "LA",
+        "MM",
+        "KP",
+        "PG",
+        "PH",
+        "KR",
+        "ID",
+        "CN",
+        "MV",
+        "NC",
+        "VU",
+        "NR"
+      ]
+    },
+    "5": {
+      name: "North Asia",
+      states: [
+        "MN",
+        "RU",
+        "KZ"
+      ]
+    }
+  }
+};
